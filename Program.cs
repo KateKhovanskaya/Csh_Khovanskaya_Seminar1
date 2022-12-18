fa4e93d18731c7e2f4824ec8c167374fa02abcf5
Kate <KhovanskayaK@yandex.ru>
2022-12-18 12:29:53 +0300

    добавлена задача 3

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
 ﻿// Задача 1. Определить, какое из двух чисел максимальное
 // Console.WriteLine("Введите первое число");
-// int num1=int.Parse(Console.ReadLine());
+// int num1=int.Parse(Console.ReadLine()!);
 // Console.WriteLine("Введите второе число");
-// int num2=int.Parse(Console.ReadLine());
+// int num2=int.Parse(Console.ReadLine()!);
 // if(num1>num2) Console.WriteLine($"Число {num1} больше");
 // else if(num1<num2) Console.WriteLine($"Число {num2} больше");
 //     else Console.WriteLine($"Числа равны");
 
 //Задача 2. Определить максимальное из трех чисел
-Console.WriteLine("Введите первое число");
-int num1=int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int num2=int.Parse(Console.ReadLine());
-Console.WriteLine("Введите третье число");
-int num3=int.Parse(Console.ReadLine());
-int max=num1;
-if (num2>max) max=num2;
-if (num3>max) max=num3;
-Console.WriteLine($"максимальное число: {max}");
+// Console.WriteLine("Введите первое число");
+// int num21=int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число");
+// int num22=int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите третье число");
+// int num23=int.Parse(Console.ReadLine()!);
+// int max=num21;
+// if (num22>max) max=num22;
+// if (num23>max) max=num23;
+// Console.WriteLine($"максимальное число: {max}");
+
+//Задача 3. Проверка, является ли число четным
+Console.WriteLine("Введите число");
+int num31=int.Parse(Console.ReadLine()!);
+if(num31%2==0) Console.WriteLine($"Число {num31} четное");
+else Console.WriteLine($"Число {num31} нечетное");
